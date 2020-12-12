1614f93c9f400f2c75ad6b80003983cf411bca2d
范轶洁 <fanyjb@digitalchina.com>
2020-12-12 16:18:36 +0800

    api url rollback

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,8 +20,7 @@ module.exports = {
     {
       resolve: "gatsby-source-strapi",
       options: {
-        apiURL:
-          process.env.API_URL || "https://dc-frontend-strapi.herokuapp.com/",
+        apiURL: process.env.API_URL || "http://localhost:1337",
         contentTypes: [
           // List of the Content Types you want to be able to request from Gatsby.
           "story",
